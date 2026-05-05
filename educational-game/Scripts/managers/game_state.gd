666a203c53899b3415da29f0a7e0637b2bb6538c
Albert Cerfeda <39586706+cerfedino@users.noreply.github.com>
2026-05-05 14:57:29 +0200

    feat: add trigger shop button in debug panel

--- educational-game/Scripts/managers/game_state.gd
+++ educational-game/Scripts/managers/game_state.gd
@@ -3,3 +3,7 @@ extends Node
 # Put game state in here. Statically accessible.
 
 var current_stage_index: int = 0
+
+func _ready() -> void:
+	Debug.add_separator("Scenes")
+	Debug.add_button("Open Shop", func(): get_tree().change_scene_to_file("res://Scenes/Shop.tscn"))
